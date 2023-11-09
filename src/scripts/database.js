8ffcbad06403d8f875c16b8301e3c3dda1896804
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2023-11-09 13:51:01 -0300

    estrutura criada

---
+++ src/scripts/database.js
@@ -0,0 +1,18 @@
+export const items = [
+    {
+        id: 1,
+        name: "Actionable insights",
+        description: "Optimize your products, improve customer satisfaction and stay ahead of the competition with our product data analytics."
+    },
+    {
+        id: 2,
+        name: "Data-driven decisions",
+        description: "Make data-driven decisions with our product data analytics. Our AI-generated reports help you unlock insights hidden in your product data."
+    },
+    {
+        id: 3,
+        name: "Always affordable",
+        description: "Always affordable pricing that scales with your business. Get top-quality product data analytics services without hidden costs or unexpected fees."
+    }
+
+]
