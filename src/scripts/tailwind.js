ed6360a69345366caf7cd89a18bc40c8aef7fc5f
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2023-11-20 20:17:09 -0300

    tailwind inserido

---
+++ src/scripts/tailwind.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'headlineL': ['Fraunces', 'serif',],
+            'parag': ['Manrope'],            
+        },        
+      extend: {        
+        colors: {
+          purple1000: '#24053E',
+          ghostWhite: '#FCF8FF',
+          eucaliptus: '#44FFA1',
+        }
+      }
+    }
+  }
